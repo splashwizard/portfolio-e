31e5c615fbb1d2a83a049afbaf7afc4a9a939c6b
splashwizard <splashwizard9@gmail.com>
2024-03-05 02:19:38 -0500

    feat: update image

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,7 +5,7 @@ module.exports = {
     title: 'Mileiko Arturs',
     description:
       'Mileiko Arturs is a software engineer who specializes in building (and occasionally designing) exceptional digital experiences.',
-    siteUrl: 'https://brittanychiang.com', // No trailing slash allowed!
+    siteUrl: 'https://mileiko.vercel.app/', // No trailing slash allowed!
     image: '/og.png', // Path to your image you placed in the 'static' folder
     twitterUsername: '@bchiang7',
   },
